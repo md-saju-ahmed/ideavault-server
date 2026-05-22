04b8fb837d4cedf11cd3f9cca6751962928c7baa
md-saju-ahmed <hellomdsajuahmed@gmail.com>
2026-05-22 23:19:33 +0600

    setup express server with mongodb connection

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
 const express = require("express");
-const app = express();
-app.use(express.json());
+const dotenv = require("dotenv");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
+dotenv.config();
+
+const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(express.json());
+
+// MongoDB
+const client = new MongoClient(process.env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+// server run function
+async function run() {
+  try {
+    await client.connect();
+    console.log("MongoDB connected successfully");
+  } catch (err) {
+    console.error("Failed to connect to MongoDB:", err);
+  }
+}
+
+run();
+
+app.get("/", (req, res) => res.send("IdeaVault server running..."));
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
